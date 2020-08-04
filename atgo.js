8c78b82f2db1cacb96b4c7596cae8f3b2ca5a7c7
andromang <66984579+andromang@users.noreply.github.com>
2020-08-04 10:36:23 +0800

    Update atgo.js

---
+++ atgo.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '00e9c27f95277fb53f8b20cf3d26a57a',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://x40u1vj75ks9.com/00e9c27f95277fb53f8b20cf3d26a57a/invoke.js"></scr' + 'ipt>');
