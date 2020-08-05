d7766ad966bfb71d9e2e87f4fa25da05d848e87e
andromang <66984579+andromang@users.noreply.github.com>
2020-08-05 15:38:57 +0800

    Update hisgomis.js

--- hisgomis.js
+++ hisgomis.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">var _Hasync= _Hasync|| [];
+var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4434454,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
