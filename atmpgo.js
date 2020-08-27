423b8783ea256817d2b186fa2cc1a1cd9a4908b7
andromang <66984579+andromang@users.noreply.github.com>
2020-08-27 13:44:14 +0800

    Update atmpgo.js

--- atmpgo.js
+++ atmpgo.js
@@ -1,1 +1,8 @@
-
+atOptions = {
+		'key' : 'f8f88362e0ea5fbd85afcb8f265852c0',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://x40u1vj75ks9.com/f8f88362e0ea5fbd85afcb8f265852c0/invoke.js"></scr' + 'ipt>');
