dbd66e0c84b3323d18b6253c60e636d4166c6eb0
andromang <66984579+andromang@users.noreply.github.com>
2020-08-28 14:43:50 +0800

    Update bawmpgo.js

--- bawmpgo.js
+++ bawmpgo.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : '5fa243c8061f11f4d3701e7da084983c',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://x40u1vj75ks9.com/5fa243c8061f11f4d3701e7da084983c/invoke.js"></scr' + 'ipt>');
+
