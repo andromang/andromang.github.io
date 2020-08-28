dd80cf2548dd2892626ff79429fb9c36c03d5c39
andromang <66984579+andromang@users.noreply.github.com>
2020-08-28 14:47:20 +0800

    Update atgomis.js

--- atgomis.js
+++ atgomis.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : 'c0a8a99f155abd38d17acd340d836c98',
-		'format' : 'iframe',
-		'height' : 50,
-		'width' : 320,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://x40u1vj75ks9.com/c0a8a99f155abd38d17acd340d836c98/invoke.js"></scr' + 'ipt>');
+
