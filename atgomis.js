27670b8bc228bd06561320fc16a487f58226da28
andromang <66984579+andromang@users.noreply.github.com>
2020-09-07 23:43:41 +0800

    Update atgomis.js

--- atgomis.js
+++ atgomis.js
@@ -1,8 +1,17 @@
-atOptions = {
-		'key' : 'c0a8a99f155abd38d17acd340d836c98',
+<div id='fixedban' style='width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;upper:0;z-index:999;-webkit-transform:translateZ(0);'>
+<div>
+<a id='close-fixedban' onclick='document.getElementById("fixedban").style.display = "none";' style='cursor:pointer;'><img alt='close' src='https://1.bp.blogspot.com/-_A83iDM6JYc/VhtxROLILrI/AAAAAAAADK4/aM4ikIA6aqI/s1600/btn_close.gif' title='close button' style='vertical-align:middle;'/></a></div>
+<div style='text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto'>
+ 
+<script type="text/javascript">
+	atOptions = {
+		'key' : '4a6645b1ddac68ee11e93670fdf1347c',
 		'format' : 'iframe',
 		'height' : 50,
 		'width' : 320,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://x40u1vj75ks9.com/c0a8a99f155abd38d17acd340d836c98/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://x40u1vj75ks9.com/4a6645b1ddac68ee11e93670fdf1347c/invoke.js"></scr' + 'ipt>');
+</script>
+</div>
+</div>
