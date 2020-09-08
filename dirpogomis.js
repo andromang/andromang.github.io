7debb39a4182f22f99f3f906078a6b3191f3bcce
andromang <66984579+andromang@users.noreply.github.com>
2020-09-08 11:00:25 +0800

    Create dirpogomis.js

---
+++ dirpogomis.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+	{
+	  $('body').addClass('xepo_ads');
+	});
+
+	$(document).on('click','.xepo_ads',function(e)
+	{
+	    $(this).removeClass('xepo_ads');
+	    window.open('https://x40u1vj75ks9.com/abp9rxpru?key=244f42e0fc3af9f066e34636927e676f', '_blank');
+});
