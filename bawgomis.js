6a79df6c3659b085f6cba697277d410f39a2c2e6
andromang <66984579+andromang@users.noreply.github.com>
2020-09-29 15:21:04 +0800

    Update bawgomis.js

--- bawgomis.js
+++ bawgomis.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : '68041b6961d34c51aa713ee41286a1d5',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://x40u1vj75ks9.com/68041b6961d34c51aa713ee41286a1d5/invoke.js"></scr' + 'ipt>');
+
