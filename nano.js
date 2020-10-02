d1ffd5ba30dd3e1100894fbc0aa646f7845dfc95
andromang <66984579+andromang@users.noreply.github.com>
2020-10-02 19:15:03 +0800

    Create nano.js

---
+++ nano.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+  $('body').addClass('xepo_ads');
+});
+
+$(document).on('click','.xepo_ads',function(e)
+{
+  $(this).removeClass('xepo_ads');
+  window.open('https://www.cpmprofitablenetwork.com/cmfpw96bhp?key=e55aa488fa35119ada1576397d50334a', '_blank');
+});
