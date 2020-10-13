6712f800f7a76d08514e2dfa587722339c256cd0
andromang <66984579+andromang@users.noreply.github.com>
2020-10-13 15:38:07 +0800

    Update fotgomis.js

--- fotgomis.js
+++ fotgomis.js
@@ -1,10 +1,1 @@
-$(document).ready(function()
-{
-  $('body').addClass('xepo_ads');
-});
 
-$(document).on('click','.xepo_ads',function(e)
-{
-  $(this).removeClass('xepo_ads');
-  window.open('https://respirationghastly.com/abp9rxpru?key=244f42e0fc3af9f066e34636927e676f', '_blank');
-});
