065fcfdbcc14a63278f1bbec4ba6b24d39fb0d36
andromang <66984579+andromang@users.noreply.github.com>
2020-10-15 15:35:27 +0800

    Update fotshgo.js

--- fotshgo.js
+++ fotshgo.js
@@ -1,10 +1,1 @@
-$(document).ready(function()
-{
-  $('body').addClass('xepo_ads');
-});
 
-$(document).on('click','.xepo_ads',function(e)
-{
-  $(this).removeClass('xepo_ads');
-  window.open('https://www.bestcpmnetwork.com/wcaws98grh?key=9c2eb96f97ce01466efad87e60b1b6cc', '_blank');
-});
