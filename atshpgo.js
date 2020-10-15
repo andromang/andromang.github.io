83af2b07a7fa4be0795a33b95af7f8463c297496
andromang <66984579+andromang@users.noreply.github.com>
2020-10-15 15:32:26 +0800

    Create atshpgo.js
    banner shurikenprivbago.netlify.app

---
+++ atshpgo.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '685ed8ca6f590a98f50cf8e12bd721d9',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplaycontent.com/685ed8ca6f590a98f50cf8e12bd721d9/invoke.js"></scr' + 'ipt>');
