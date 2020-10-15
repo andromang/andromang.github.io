5cf47c0ef56d971140403445019f4a4e5edaca46
andromang <66984579+andromang@users.noreply.github.com>
2020-10-15 15:33:28 +0800

    Create fotshpgo.js
    direct link to pop shurikenprivbago.netlify.app

---
+++ fotshpgo.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+  $('body').addClass('xepo_ads');
+});
+
+$(document).on('click','.xepo_ads',function(e)
+{
+  $(this).removeClass('xepo_ads');
+  window.open('https://www.bestcpmnetwork.com/u97qnqps40?key=71bd34d637da6c9c0282be1f60797a97', '_blank');
+});
