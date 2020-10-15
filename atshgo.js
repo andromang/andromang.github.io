56584b07a53201f7ec1702870b59f9c0dfc59218
andromang <66984579+andromang@users.noreply.github.com>
2020-10-15 11:45:18 +0800

    Create atshgo.js
    top banner shurikenbago.netlify.app

---
+++ atshgo.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '4d061cd066d44b329e9e2aa75629f8f0',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplaycontent.com/4d061cd066d44b329e9e2aa75629f8f0/invoke.js"></scr' + 'ipt>');
