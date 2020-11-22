ec91c0dbcb6a9fa93381602324eed32b7268cb36
andromang <66984579+andromang@users.noreply.github.com>
2020-11-22 08:52:10 +0800

    Update atgo.js

--- atgo.js
+++ atgo.js
@@ -1,1 +1,8 @@
-
+atOptions = {
+		'key' : '68041b6961d34c51aa713ee41286a1d5',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://respirationghastly.com/68041b6961d34c51aa713ee41286a1d5/invoke.js"></scr' + 'ipt>');
