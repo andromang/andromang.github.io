2a5a795262470f05417142d3ae519fe8001e90c9
andromang <66984579+andromang@users.noreply.github.com>
2021-03-09 21:02:42 +0800

    Update fotshpgo.js

--- fotshpgo.js
+++ fotshpgo.js
@@ -1,9 +1,12 @@
-$(document).ready(function()
-	{
-	  $('body').addClass('xepo_ads');
-	});
+src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js';
 
-	$(document).on('click','.xepo_ads',function(e)
-	{
-	    $(this).removeClass('xepo_ads');
-	    window.open('https://www.profitabletrustednetwork.com/jp7wt9skzt?key=914f151324404eff1a147945dd4faaff', '_blank');
+$(document).ready(function()
+{
+  $('body').addClass('xepo_ads');
+});
+
+$(document).on('click','.xepo_ads',function(e)
+{
+  $(this).removeClass('xepo_ads');
+  window.open('https://www.profitabletrustednetwork.com/jp7wt9skzt?key=914f151324404eff1a147945dd4faaff', '_blank');
+});
