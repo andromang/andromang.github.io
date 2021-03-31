6dc085f1ceafb2b75d566a430f41c663926616d5
andromang <66984579+andromang@users.noreply.github.com>
2021-04-01 03:25:20 +0800

    Update atshgo.js

--- atshgo.js
+++ atshgo.js
@@ -1,1 +1,8 @@
-
+	atOptions = {
+		'key' : 'ff2b98099db015a1539b6f1ce25ec5ce',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.creativedisplayformat.com/ff2b98099db015a1539b6f1ce25ec5ce/invoke.js"></scr' + 'ipt>');
