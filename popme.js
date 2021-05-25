4896f21095ff9aec5be891921bb4dd6364234ec3
andromang <66984579+andromang@users.noreply.github.com>
2021-05-25 09:34:21 +0800

    Update and rename popmpgo.js to popme.js

---
+++ popme.js
@@ -0,0 +1,41 @@
+var go_current     	= window.location.href;
+    var reff     	= document.referrer;
+    
+
+    function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+    var direct_link_ads = rChoice([
+                            "https://www.achievablecpmrevenue.com/cmfpw96bhp?key=e55aa488fa35119ada1576397d50334a",
+                        ]);
+
+    var ars             = rChoice([
+                            "https://xobbcoop.net",
+                        ]);
+
+
+    var dir_type        = 'arsae'; //refresh, domain, path, arsae
+
+
+    if(dir_type == 'refresh')
+    {
+        //REFRESH
+        console.log('refresh..');
+    }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
