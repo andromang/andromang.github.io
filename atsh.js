0532891fc97e828992f8dda50d951c6d193e6634
andromang <66984579+andromang@users.noreply.github.com>
2021-05-30 06:59:22 +0800

    Update atsh.js

--- atsh.js
+++ atsh.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '68041b6961d34c51aa713ee41286a1d5',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://respirationghastly.com/68041b6961d34c51aa713ee41286a1d5/invoke.js"></scr' + 'ipt>');
+	'key' : 'ff2b98099db015a1539b6f1ce25ec5ce',
+	'format' : 'iframe',
+	'height' : 250,
+	'width' : 300,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.creativeformatsnetwork.com/ff2b98099db015a1539b6f1ce25ec5ce/invoke.js"></scr' + 'ipt>');
