3c691379bd16449ff5335478b45911233886fab6
andromang <66984579+andromang@users.noreply.github.com>
2022-11-01 14:28:01 +0800

    Update and rename popgo.js to bawgomis.js

---
+++ bawgomis.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '556607bed130670877d5708a72228bf7',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/556607bed130670877d5708a72228bf7/invoke.js"></scr' + 'ipt>');
