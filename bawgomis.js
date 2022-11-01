a9c1ebb68095bd73de6ddfb42fee64f0a2d40c31
andromang <66984579+andromang@users.noreply.github.com>
2022-11-01 16:03:27 +0800

    Update bawgomis.js

--- bawgomis.js
+++ bawgomis.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : 'cefc8371ecd891211afd4ade70446c55',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/cefc8371ecd891211afd4ade70446c55/invoke.js"></scr' + 'ipt>');
+
