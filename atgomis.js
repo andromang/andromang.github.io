1e8e68487d6a4f06afbed04ed6b2adec9101ef8c
andromang <66984579+andromang@users.noreply.github.com>
2022-11-02 23:03:23 +0800

    Update atgomis.js

--- atgomis.js
+++ atgomis.js
@@ -1,1 +1,8 @@
-
+atOptions = {
+		'key' : '3961efa56d20560d2d1be88f3305b433',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/3961efa56d20560d2d1be88f3305b433/invoke.js"></scr' + 'ipt>');
