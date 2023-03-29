e32ccbf024acfb961f335244d4101b3e1cff8e81
andromang <66984579+andromang@users.noreply.github.com>
2023-03-29 14:52:31 +0800

    Update bawgomis.js

--- bawgomis.js
+++ bawgomis.js
@@ -1,1 +1,8 @@
-
+	atOptions = {
+		'key' : 'c0a8a99f155abd38d17acd340d836c98',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/c0a8a99f155abd38d17acd340d836c98/invoke.js"></scr' + 'ipt>');
