9fc06d027a3a76259a51f494042cec9b62517600
andromang <66984579+andromang@users.noreply.github.com>
2025-10-24 07:32:23 +0800

    Create bawgo.js

---
+++ bawgo.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '46ae6ddb6151271d318313f84463d461',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/46ae6ddb6151271d318313f84463d461/invoke.js"></scr' + 'ipt>');
