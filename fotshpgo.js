9aad441c81254e4034fc1c32aa964838ad79a534
andromang <66984579+andromang@users.noreply.github.com>
2025-10-24 07:38:18 +0800

    Update fotshpgo.js

--- fotshpgo.js
+++ fotshpgo.js
@@ -8,5 +8,5 @@ $(document).ready(function()
 $(document).on('click','.xepo_ads',function(e)
 {
   $(this).removeClass('xepo_ads');
-  window.open('https://www.profitabletrustednetwork.com/jp7wt9skzt?key=914f151324404eff1a147945dd4faaff', '_blank');
+  window.open('https://https://www.effectivegatecpm.com/jp7wt9skzt?key=914f151324404eff1a147945dd4faaff', '_blank');
 });
