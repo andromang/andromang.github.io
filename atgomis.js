9da3e3c10da0fda751dda505d4e60af7930665bd
andromang <66984579+andromang@users.noreply.github.com>
2025-10-24 07:27:57 +0800

    Create atgomis.js

---
+++ atgomis.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '5b53fd19e08514bb440ee051cf528722',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/5b53fd19e08514bb440ee051cf528722/invoke.js"></scr' + 'ipt>');
