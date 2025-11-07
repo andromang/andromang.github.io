0d9445963de6d4ef0e373a1d278429f0d6839571
andromang <66984579+andromang@users.noreply.github.com>
2025-11-07 13:01:46 +0800

    Update fotshpgo.js

--- fotshpgo.js
+++ fotshpgo.js
@@ -1,12 +1,16 @@
-src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js';
+src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js';
 
-$(document).ready(function()
-{
+$(document).ready(function () {
   $('body').addClass('xepo_ads');
 });
 
-$(document).on('click','.xepo_ads',function(e)
-{
+$(document).on('click', '.xepo_ads', function (e) {
   $(this).removeClass('xepo_ads');
-  window.open('https://https://www.effectivegatecpm.com/jp7wt9skzt?key=914f151324404eff1a147945dd4faaff', '_blank');
+
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://js.wpadmngr.com/static/adManager.js';
+  s.dataset.admpid = '386739';
+  
+  document.body.appendChild(s);
 });
