2ccf11c835341bbf3ff5b5d95cafd2b33884e82d
andromang <66984579+andromang@users.noreply.github.com>
2025-11-19 16:38:23 +0800

    Update kampnewnv.js

--- kampnewnv.js
+++ kampnewnv.js
@@ -1,14 +1,22 @@
 (function () {
 
+    // Cari script yang memanggil file ini
+    var currentScript = document.currentScript;
+
+    // Buat container untuk iklan
     var container = document.createElement("div");
     container.id = "container-dcb12e0c77bf600afc906d0bf9fb6070";
-    document.body.appendChild(container);
 
+    // Sisipkan sebelum tag script yang memanggil file ini
+    currentScript.parentNode.insertBefore(container, currentScript);
+
+    // Load script iklan EffectiveGate
     var script = document.createElement("script");
     script.async = true;
     script.setAttribute("data-cfasync", "false");
     script.src = "//pl28061824.effectivegatecpm.com/dcb12e0c77bf600afc906d0bf9fb6070/invoke.js";
 
-    document.body.appendChild(script);
+    // Sisipkan setelah container
+    currentScript.parentNode.insertBefore(script, currentScript.nextSibling);
 
 })();
