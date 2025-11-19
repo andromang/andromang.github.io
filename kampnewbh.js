5a6180f05180030f8184020915616847bb823a3f
andromang <66984579+andromang@users.noreply.github.com>
2025-11-19 16:26:08 +0800

    Update kampnewbh.js

--- kampnewbh.js
+++ kampnewbh.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '5b53fd19e08514bb440ee051cf528722',
+		'key' : '5c8763ccd6a114c3eb5ffa478379734a',
 		'format' : 'iframe',
 		'height' : 250,
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/5b53fd19e08514bb440ee051cf528722/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/5c8763ccd6a114c3eb5ffa478379734a/invoke.js"></scr' + 'ipt>');
