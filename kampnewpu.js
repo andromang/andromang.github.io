4b49cea304087f64788f96a8e1ae72d44a42d2a8
andromang <66984579+andromang@users.noreply.github.com>
2025-11-19 17:14:36 +0800

    Update kampnewpu.js

--- kampnewpu.js
+++ kampnewpu.js
@@ -1,1 +1,6 @@
-
+(function() {
+    var s = document.createElement('script');
+    s.src = "//pl28087072.effectivegatecpm.com/bd/60/7e/bd607e3d28a1bf5db6ab8f7918f00bea.js";
+    s.async = true;
+    document.head.appendChild(s);
+})();
