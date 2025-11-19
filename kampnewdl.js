08f32a24e8b41de63124b74cc136de072c1170c9
andromang <66984579+andromang@users.noreply.github.com>
2025-11-19 16:59:21 +0800

    Update kampnewdl.js

--- kampnewdl.js
+++ kampnewdl.js
@@ -1,22 +1,8 @@
-(function () {
-
-    // Cari script yang memanggil file ini
-    var currentScript = document.currentScript;
-
-    // Buat container untuk iklan
-    var container = document.createElement("div");
-    container.id = "container-dcb12e0c77bf600afc906d0bf9fb6070";
-
-    // Sisipkan sebelum tag script yang memanggil file ini
-    currentScript.parentNode.insertBefore(container, currentScript);
-
-    // Load script iklan EffectiveGate
-    var script = document.createElement("script");
-    script.async = true;
-    script.setAttribute("data-cfasync", "false");
-    script.src = "//pl28061824.effectivegatecpm.com/dcb12e0c77bf600afc906d0bf9fb6070/invoke.js";
-
-    // Sisipkan setelah container
-    currentScript.parentNode.insertBefore(script, currentScript.nextSibling);
-
-})();
+atOptions = {
+		'key' : '5b53fd19e08514bb440ee051cf528722',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/5b53fd19e08514bb440ee051cf528722/invoke.js"></scr' + 'ipt>');
