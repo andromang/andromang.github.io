83d3f5c9d021fb0642dc53764c64932e2fc6ecaa
andromang <66984579+andromang@users.noreply.github.com>
2025-11-19 15:49:24 +0800

    Update kampnewbf.js

--- kampnewbf.js
+++ kampnewbf.js
@@ -1,1 +1,13 @@
+window.atOptions = {
+    'key': 'b6226b8273fb8c5618b57a1cc54c7b86',
+    'format': 'iframe',
+    'height': 250,
+    'width': 300,
+    'params': {}
+};
 
+var adScript = document.createElement("script");
+adScript.type = "text/javascript";
+adScript.src = "//www.highperformanceformat.com/b6226b8273fb8c5618b57a1cc54c7b86/invoke.js";
+
+(document.head || document.body).appendChild(adScript);
