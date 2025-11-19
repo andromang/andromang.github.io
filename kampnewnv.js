d54b7b7a854100fa7998968148f49cc61cd0ca30
andromang <66984579+andromang@users.noreply.github.com>
2025-11-19 16:32:40 +0800

    Update kampnewnv.js

--- kampnewnv.js
+++ kampnewnv.js
@@ -1,1 +1,14 @@
+(function () {
 
+    var container = document.createElement("div");
+    container.id = "container-dcb12e0c77bf600afc906d0bf9fb6070";
+    document.body.appendChild(container);
+
+    var script = document.createElement("script");
+    script.async = true;
+    script.setAttribute("data-cfasync", "false");
+    script.src = "//pl28061824.effectivegatecpm.com/dcb12e0c77bf600afc906d0bf9fb6070/invoke.js";
+
+    document.body.appendChild(script);
+
+})();
